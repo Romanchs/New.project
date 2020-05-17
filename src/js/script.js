c330a7b9a2a91776ee744836146978bc399bda27
Maksym <rkorj32@gmail.com>
2020-05-17 18:00:21 +0300

    in work

---
+++ src/js/script.js
@@ -0,0 +1,59 @@
+var name = "Max";
+
+let number = 7;
+const pi = 3.14;
+
+let leftBorderWidth = 200;
+
+// let obj = {
+//     name = "apple",
+//     color = "green",
+//     weight = "200"
+// }
+
+// console.log(number);
+
+// let answer = confirm ("18+?");
+// console.log(answer);
+
+// let answer = prompt ("18+?", "");
+// console.log(answer);
+
+// let isChecked = true,
+//     isClosed = false;
+
+// console.log(isChecked && isClosed);
+// console.log(isChecked || isClosed);
+
+// if (2*4 == 8*12) {
+//     console.log("True")
+// } else {
+//     console.log("Error")
+// }
+
+// let answer = confirm("18+?");
+// if (answer) {
+//     console.log("Go")
+// } else {
+//     console.log("Get out")
+// }
+
+// const num = 50;
+
+// if (num < 49) {
+//     console.log("Error")
+// } else if (num > 100) {
+//     console.log("Many")
+// } else {
+//     console.log("True")
+// }
+
+// for(let i = 1; i < 8; i++) {
+//     console.log(i);
+// }
+
+function logging(a, b) {
+    console.log(a + b)
+}
+
+logging (3, 5);
