57f24049bd7f65e6458768824513b87936110f17
Maksym <rkorj32@gmail.com>
2020-05-18 17:30:13 +0300

    slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,59 +1,16 @@
-var name = "Max";
-
-let number = 7;
-const pi = 3.14;
-
-let leftBorderWidth = 200;
-
-// let obj = {
-//     name = "apple",
-//     color = "green",
-//     weight = "200"
-// }
-
-// console.log(number);
-
-// let answer = confirm ("18+?");
-// console.log(answer);
-
-// let answer = prompt ("18+?", "");
-// console.log(answer);
-
-// let isChecked = true,
-//     isClosed = false;
-
-// console.log(isChecked && isClosed);
-// console.log(isChecked || isClosed);
-
-// if (2*4 == 8*12) {
-//     console.log("True")
-// } else {
-//     console.log("Error")
-// }
-
-// let answer = confirm("18+?");
-// if (answer) {
-//     console.log("Go")
-// } else {
-//     console.log("Get out")
-// }
-
-// const num = 50;
-
-// if (num < 49) {
-//     console.log("Error")
-// } else if (num > 100) {
-//     console.log("Many")
-// } else {
-//     console.log("True")
-// }
-
-// for(let i = 1; i < 8; i++) {
-//     console.log(i);
-// }
-
-function logging(a, b) {
-    console.log(a + b)
-}
-
-logging (3, 5);
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1000,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/arrow-left.svg" alt="left"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/arrow-right.svg" alt="right"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+    });
+});
